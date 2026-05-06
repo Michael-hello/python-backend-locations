30cabe658e2dd84af3f6653c097105e86a1afe50
Mike Jones <mjones@robertson-geo.com>
2026-05-06 07:52:18 +0100

    experiment with allowing all CORS requests

--- app/main.py
+++ app/main.py
@@ -33,9 +33,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_origin_regex=r"https://michael-hello\.github\.io/.*",
-    allow_credentials=True,
+    allow_origins="*",
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
